3c763ea2581e78072d506070d7bad1f23fe55178
Mata Vivien-Jorix <vivien-jorix.mata@etudiant.univ-lille1.fr>
2018-03-16 09:35:28 +0100

    recuperation user

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -26,9 +26,9 @@ export class RestProvider {
     });
   }
 
-  getDeal(choice:String) {
+  getDeal(choice, lat, long) {
     return new Promise(resolve => {
-      this.http.get(this.apiUrl+'/Read.php?type=' + choice).subscribe(data => {
+      this.http.get(this.apiUrl+'/Read.php?type=' + choice+'&latitude='+lat+'&longitude='+long).subscribe(data => {
         resolve(data);
       }, err => {
         console.log("err:"+err.status);
@@ -85,6 +85,42 @@ export class RestProvider {
   readFilter(filter) {
     return new Promise(resolve => {
       this.http.get(this.apiUrl+'/ReadFilter.php?type=' + filter.type + '&categorie_restaurant=' + filter.categorie_restaurant).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log("err:"+err.status);
+      });
+    });
+  }
+
+  addFavorite(deal, user) {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/CreateFavoris.php?id_deal=' + deal + '&id_client=' + user).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log("err:"+err.status);
+      });
+    });
+  }
+
+  createCommentaire(data){
+    data = {
+      id_deal:44,
+      id_client: 1,
+      com: 'SALUUUUT'
+    }
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiUrl+'/CreateCommentaire.php', JSON.stringify(data))
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  getAllCommentaire(id){
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/ReadCommentairesDeal.php?id_deal=' + id).subscribe(data => {
         resolve(data);
       }, err => {
         console.log("err:"+err.status);
